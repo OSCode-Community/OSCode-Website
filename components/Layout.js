bde497a078e898780ef1d21c0aeef07370df2c0e
FuryACE <sudhanshuvshekhar@gmail.com>
2022-06-12 02:52:16 +0530

    design tweaks

--- components/Layout.js
+++ components/Layout.js
@@ -4,12 +4,27 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Avatar from "@mui/material/Avatar";
-
+import MenuIcon from "@mui/icons-material/Menu";
 import Link from "next/link";
 
 import classes from "./Layout.module.css";
 
 const Layout = (props) => {
+  const navItems = [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "About Us",
+      href: "/about",
+    },
+    {
+      title: "Events",
+      href: "/events",
+    },
+  ];
+
   return (
     <>
       <Box sx={{ background: "black" }}>
@@ -19,8 +34,8 @@ const Layout = (props) => {
               src="/logo.jpeg"
               alt="oscode logo"
               sx={{
-                width: "50px",
-                height: "50px",
+                width: { xs: "2rem", md: "3rem" },
+                height: { xs: "2rem", md: "3rem" },
                 marginRight: "12px",
               }}
             />
@@ -29,39 +44,29 @@ const Layout = (props) => {
                 OS Code
               </Typography>
             </Link>
-            <Link href="/">
-              <Button
-                color="inherit"
-                edge="end"
-                sx={{ mx: 1 }}
-                className={classes.btn}
-              >
-                Home
-              </Button>
-            </Link>
-            <Link href="/about">
-              <Button
-                color="inherit"
-                edge="end"
-                sx={{ mx: 1 }}
-                className={classes.btn}
-              >
-                About Us
-              </Button>
-            </Link>
-            <Link href="/events">
-              <Button
-                color="inherit"
-                edge="end"
-                sx={{ mx: 1 }}
-                className={classes.btn}
-              >
-                Events
-              </Button>
-            </Link>
-            <Link href="/contact">
-              <Button edge="end" sx={{ mx: 1 }} variant="outlined"> Contact Us </Button> 
-            </Link>
+
+            <div className={classes.navItemsLg}>
+              {navItems.map((item) => (
+                <Link href={item.href} key={item.title}>
+                  <Button
+                    color="inherit"
+                    edge="end"
+                    sx={{ mx: 1 }}
+                    className={classes.btn}
+                  >
+                    {item.title}
+                  </Button>
+                </Link>
+              ))}
+              <Link href="/contact">
+                <Button edge="end" sx={{ mx: 1 }} variant="outlined">
+                  {" "}
+                  Contact Us{" "}
+                </Button>
+              </Link>
+            </div>
+
+            
           </Toolbar>
         </AppBar>
         <main style={{ paddingTop: "60px" }}>{props.children}</main>
@@ -70,11 +75,7 @@ const Layout = (props) => {
           <Typography variant="h6" align="center" gutterBottom>
             OS Code
           </Typography>
-          <Typography
-            variant="subtitle1"
-            align="center"
-            component="p"
-          >
+          <Typography variant="subtitle1" align="center" component="p">
             © {new Date().getFullYear()}, Made by Team OS Code
           </Typography>
         </footer>
