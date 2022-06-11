7eac62a804e78fe3a1027cfcef02d38a65204f58
FuryACE <sudhanshuvshekhar@gmail.com>
2022-06-12 03:20:09 +0530

    added drawer component

--- components/Layout.js
+++ components/Layout.js
@@ -4,12 +4,16 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Avatar from "@mui/material/Avatar";
-import MenuIcon from "@mui/icons-material/Menu";
 import Link from "next/link";
-
 import classes from "./Layout.module.css";
+import MenuIcon from '@mui/icons-material/Menu';
+import { IconButton } from "@mui/material";
+import { useState } from "react";
+import { Drawer } from "@mui/material";
 
 const Layout = (props) => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
   const navItems = [
     {
       title: "Home",
@@ -65,8 +69,36 @@ const Layout = (props) => {
                 </Button>
               </Link>
             </div>
-
-            
+            <IconButton size="large" edge="start" color="inherit" aria-label="logo" onClick={() => setIsDrawerOpen(true)}>
+              <MenuIcon />
+            </IconButton>
+            <Drawer
+              anchor="right"
+              open={isDrawerOpen}
+              onClose={() => setIsDrawerOpen(false)}
+              sx={{ display: { sx: "flex", md: "none" } }}
+            >
+              <Box p={2} textAlign="center" role="presentation">
+                {navItems.map((item) => (
+                  <Link href={item.href} key={item.title}>
+                    <Button
+                      color="inherit"
+                      edge="end"
+                      sx={{ mx: 1 }}
+                      className={classes.btn}
+                    >
+                      {item.title}
+                    </Button>
+                  </Link>
+                ))}
+                <Link href="/contact">
+                  <Button edge="end" sx={{ mx: 1 }} variant="outlined">
+                    {" "}
+                    Contact Us{" "}
+                  </Button>
+                </Link>
+              </Box>
+            </Drawer>
           </Toolbar>
         </AppBar>
         <main style={{ paddingTop: "60px" }}>{props.children}</main>
