2f3b1adfd21c20184e1d5d050c9c34c90db1b1d2
FuryACE <sudhanshuvshekhar@gmail.com>
2022-06-13 01:39:56 +0530

    front page ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,37 @@ import type { NextPage } from "next";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-
 import classes from "./home.module.css";
+import { Button } from "@mui/material";
+import Link from "@mui/material/Link";
 
 const Home = () => {
   return (
-    <div className={classes.bgImage}>
-      
-    </div>
+    <Box my={20} ml={15}>
+      <Typography
+        variant="h2"
+        component="h1"
+        className={classes.heroText}
+        sx={{ marginLeft: {} }}
+        gutterBottom
+      >
+        A community of Open Source <br></br>
+        developers
+      </Typography>
+      <Typography
+        variant="h5"
+        component="h2"
+        sx={{ marginLeft: {}, fontWeight: "lighter" }}
+        color="white"
+        gutterBottom
+      >
+        Join the community and help us build the next generation of open source
+        tools.
+      </Typography>
+      <Link href="/events">
+        <Button className={classes.button}>Explore Events</Button>
+      </Link>
+    </Box>
   );
 };
 
