ee6870caf3746d4829931cb007733dc4bbfc7b96
FuryACE <sudhanshuvshekhar@gmail.com>
2022-06-13 18:35:51 +0530

    created events page

---
+++ components/EventCard.tsx
@@ -0,0 +1,59 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import classes from "./EventCard.module.css";
+import {
+  ReactElement,
+  JSXElementConstructor,
+  ReactFragment,
+  ReactPortal,
+} from "react";
+
+export default function EventCard(props: {
+  image: string | undefined;
+  title:
+    | string
+    | number
+    | boolean
+    | ReactElement<any, string | JSXElementConstructor<any>>
+    | ReactFragment
+    | ReactPortal
+    | null
+    | undefined;
+  description:
+    | string
+    | number
+    | boolean
+    | ReactElement<any, string | JSXElementConstructor<any>>
+    | ReactFragment
+    | ReactPortal
+    | null
+    | undefined;
+}) {
+  return (
+    <Card
+      sx={{ minWidth: 345, minHeight: 500 }}
+      className={classes.eventCardBg}
+    >
+      <CardMedia
+        component="img"
+        alt="event image"
+        height="300"
+        image={props.image}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.title}
+        </Typography>
+        <Typography variant="body2">{props.description}</Typography>
+      </CardContent>
+      <CardActions sx={{paddingTop: "2rem"}}>
+        <Button size="small">Register</Button>
+        <Button size="small">Rules</Button>
+      </CardActions>
+    </Card>
+  );
+}
--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -2,24 +2,61 @@ import Head from "next/head";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import classes from "./events.module.css"
+import Grid from "@mui/material/Grid";
+import classes from "./events.module.css";
+import EventCard from "../../components/EventCard";
 
 const Events = () => {
+  const events = [
+    {
+      title: "Event 1",
+      description: "This is the description of event 1",
+      image: "https://source.unsplash.com/random",
+    },
+    {
+      title: "Event 2",
+      description: "This is the description of event 2",
+      image: "https://source.unsplash.com/random",
+    },
+    {
+      title: "Event 3",
+      description: "This is the description of event 3",
+      image: "https://source.unsplash.com/random",
+    },
+  ];
   return (
     <>
       <Head>
         <title>Events</title>
         <meta name="description" content="Events" />
       </Head>
-      <Container maxWidth="lg" sx={{ color: "white", marginTop: {lg: "3rem"} }} >
+      <Container
+        maxWidth="lg"
+        sx={{ color: "white", marginTop: { lg: "3rem", xs: "5rem" } }}
+      >
         <Box>
-            <Typography variant="h2" color="primary" sx={{  }} className={classes.titleEvent}>
-                Events
-            </Typography>
-            <Typography variant="h5" py="2rem" className={classes.manthanText}>
-                Manthan 2022 Events
-            </Typography>
-
+          <Typography
+            variant="h2"
+            color="primary"
+            sx={{ paddingLeft: { sx: "10rem" } }}
+            className={classes.titleEvent}
+          >
+            Events
+          </Typography>
+          <Typography variant="h5" py="2rem" className={classes.manthanText}>
+            Manthan 2022 Event List:
+          </Typography>
+          <Grid container spacing={2} sx={{ paddingTop: "2rem" }}>
+            {events.map((event, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <EventCard
+                  image={event.image}
+                  title={event.title}
+                  description={event.description}
+                />
+              </Grid>
+            ))}
+          </Grid>
         </Box>
       </Container>
     </>
